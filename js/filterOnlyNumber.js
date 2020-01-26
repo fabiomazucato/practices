3af46cd9b1db3f77948f35a274988aaf6f37ed07
Fabio Mazucato <fabio@buildbox.com.br>
2020-01-26 17:39:06 -0300

    array return

--- js/filterOnlyNumber.js
+++ js/filterOnlyNumber.js
@@ -3,6 +3,8 @@ const arr = [1, 2, undefined, 3, null, 4, NaN, 5, '', 6]
 /**
  * Filter only numbers
  */
-const arr3 = arr.filter(value => Number(value))
+const arr2 = arr.filter(value => Number(value))
 
-const arr2 = arr.filter(Boolean)
+const arr3 = arr.filter(Boolean)
+
+// return arr2 and arr3 [1, 2, 3, 4, 5, 6]
