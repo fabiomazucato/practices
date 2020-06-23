f8f649571f05155a69807cd617661711c4266c23
Fabio Mazucato <fabiomazucato@gmail.com>
2020-06-23 16:07:33 -0300

    Prepare url params in object

--- js/prepareUrlParamsInObject.js
+++ js/prepareUrlParamsInObject.js
@@ -4,15 +4,24 @@ const url =
 const prepareUrlParamsInObject = (url) => {
 	const urlParameters = url.split('?')[1]
 
-	const parametersInProperty = decodeURI(
-		urlParameters.replace(/&/g, '","').replace(/=/g, '":"')
-	)
-
-	const objectUrlParameters = JSON.parse(`{"${parametersInProperty}"}`)
+	const objectUrlParameters = new URLSearchParams(urlParameters)
 
 	return objectUrlParameters
 }
 
 const objectUrlParameters = prepareUrlParamsInObject(url)
-
 // return { hl: 'pt-BR', sa: 'X', ved: '0ahUKEwjmk8qztZjqAhWnKLkGHYrmCuAQPAgH' }
+
+const prepareUrlParamsInObject_2 = (url) => {
+	const urlParameters = url.split('?')[1]
+
+	const objectUrlParameters = new URLSearchParams(urlParameters)
+
+	return objectUrlParameters
+}
+
+const objectUrlParameters_2 = prepareUrlParamsInObject_2(url)
+// return URLSearchParams { 'hl' => 'pt-BR', 'sa' => 'X', 'ved' => '0ahUKEwjmk8qztZjqAhWnKLkGHYrmCuAQPAgH' }
+
+const hl = objectUrlParameters_2.get('hl')
+// return pt-BR
